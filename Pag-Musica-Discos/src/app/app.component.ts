318dae2d558418ec06609e624d53548bb80a9f7c
Alberto-BS <163292435+Alberto-BS@users.noreply.github.com>
2024-04-30 11:35:27 +0200

    Pull request

--- Pag-Musica-Discos/src/app/app.component.ts
+++ Pag-Musica-Discos/src/app/app.component.ts
@@ -10,36 +10,39 @@ export class AppComponent {
   title = 'Pag-Musica-Discos';
   disco = new Disco();
   contactForm = new FormGroup({
-    imgPortada: new FormControl(),
-    fechas: new FormControl(),
+    fechas: new FormControl('', [Validators.required, this.validateFechaNacimiento]),
     artistas: new FormControl(null, [Validators.maxLength(50), Validators.minLength(2)]),
     cancion: new FormControl(),
     nombres: new FormControl(null, [Validators.maxLength(50), Validators.minLength(2)]),
   })
 
   onSubmit() {
-    this.disco.imgPortada = this.contactForm.get("imgPortada")?.value;
-    this.disco.fechas = this.contactForm.get("fechas")?.value;
+    this.disco.fechas = this.contactForm.get("fechas")?.value as unknown as string;
     this.disco.artistas = this.contactForm.get("artistas")?.value as unknown as string;
     this.disco.nombres = this.contactForm.get("nombres")?.value as unknown as string;
     console.log(this.disco.nombres);
   }
   addCancion() {
-    let cancion = this.contactForm.get("cancion")?.value;
-    this.disco.cancion.push(cancion);
-    let ul = document.getElementById("canciones");
-    for (let c of cancion) {
-      let li = document.createElement("li");
-      li.textContent = c;
-      ul?.appendChild(li);
-    }
+    this.disco.cancion.push(this.contactForm.get("cancion")?.value);
     console.log(this.disco.cancion);
   }
+
+  validateFechaNacimiento(control: FormControl): { [key: string]: boolean } | null {
+    const fechaNacimiento = new Date(control.value);
+    const hoy = new Date();
+
+    if (fechaNacimiento > hoy) {
+      return { 'fechaFutura': true };
+    }
+
+    return null;
+  }
+
+
 }
 
 
 class Disco {
-  imgPortada: string = "";
   nombres: string = "";
   fechas: string = "";
   artistas: string = "";
