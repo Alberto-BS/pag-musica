b9d635ad3302f15bfb523285156309e2295b0a1b
Alberto-BS <163292435+Alberto-BS@users.noreply.github.com>
2024-04-30 10:38:24 +0200

    Hemos puesto los validadores de algunos inputs y aun queda por solucioinar un poco los caruseles que tiene que poner pedro ell otro formulario

--- Pag-Musica-Discos/src/app/app.component.ts
+++ Pag-Musica-Discos/src/app/app.component.ts
@@ -10,33 +10,39 @@ export class AppComponent {
   title = 'Pag-Musica-Discos';
   disco = new Disco();
   contactForm = new FormGroup({
-    imgPortada: new FormControl(),
-    fechas: new FormControl(),
+    fechas: new FormControl('', [Validators.required, this.validateFechaNacimiento]),
     artistas: new FormControl(null, [Validators.maxLength(50), Validators.minLength(2)]),
     cancion: new FormControl(),
     nombres: new FormControl(null, [Validators.maxLength(50), Validators.minLength(2)]),
   })
 
   onSubmit() {
-    this.disco.imgPortada = this.contactForm.get("imgPortada")?.value;
-    this.disco.fechas = this.contactForm.get("fechas")?.value;
+    this.disco.fechas = this.contactForm.get("fechas")?.value as unknown as string;
     this.disco.artistas = this.contactForm.get("artistas")?.value as unknown as string;
     this.disco.nombres = this.contactForm.get("nombres")?.value as unknown as string;
     console.log(this.disco.nombres);
   }
   addCancion() {
     this.disco.cancion.push(this.contactForm.get("cancion")?.value);
-    let ul = document.getElementById("canciones");
-      let li = document.createElement("li");
-    li.textContent = this.contactForm.get("cancion")?.value;
-      ul?.appendChild(li);
     console.log(this.disco.cancion);
   }
+
+  validateFechaNacimiento(control: FormControl): { [key: string]: boolean } | null {
+    const fechaNacimiento = new Date(control.value);
+    const hoy = new Date();
+
+    if (fechaNacimiento > hoy) {
+      return { 'fechaFutura': true };
+    }
+
+    return null;
+  }
+
+
 }
 
 
 class Disco {
-  imgPortada: string = "";
   nombres: string = "";
   fechas: string = "";
   artistas: string = "";
